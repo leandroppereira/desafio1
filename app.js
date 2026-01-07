81b324bf9a7a42e48fdb32a328787bf57a8a39b3
Leandro Pereira <leandropantoja@gmail.com>
2026-01-06 23:23:57 -0300

    2026

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
